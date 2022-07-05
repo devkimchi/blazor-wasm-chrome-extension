9daa905290446634b8d0b53371310e76f3cb8c6f
Justin Yoo <justin.yoojh@gmail.com>
2022-07-05 13:43:36 +0900

    Update project name targeting to v2

--- src/ChromeExtensionV2/wwwroot/js/options.js
+++ src/ChromeExtensionV2/wwwroot/js/options.js
@@ -1,9 +1,12 @@
 let page = document.getElementById('buttonDiv');
+
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+
 function constructOptions(kButtonColors) {
   for (let item of kButtonColors) {
     let button = document.createElement('button');
     button.style.backgroundColor = item;
+
     button.addEventListener('click', function() {
       chrome.storage.sync.set({color: item}, function() {
         console.log('color is ' + item);
@@ -12,4 +15,5 @@ function constructOptions(kButtonColors) {
     page.appendChild(button);
   }
 }
+
 constructOptions(kButtonColors);
