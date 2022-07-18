3d25c37c0cacf9a12645566af8021b35fcc2363c
Justin Yoo <justin.yoojh@gmail.com>
2022-07-18 15:53:42 +0900

    Remove no-longer-required SHA keys from manifest.json

--- src/ChromeExtensionV2/wwwroot/js/options.js
+++ src/ChromeExtensionV2/wwwroot/js/options.js
@@ -5,7 +5,9 @@ const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
 function constructOptions(kButtonColors) {
   for (let item of kButtonColors) {
     let button = document.createElement('button');
+    button.className = 'color-button';
     button.style.backgroundColor = item;
+    button.style.padding = '10px';
 
     button.addEventListener('click', function() {
       chrome.storage.sync.set({color: item}, function() {
