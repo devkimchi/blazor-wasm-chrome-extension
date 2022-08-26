6e3a2205d3758e10f677c5f4b318ff8e230f6efa
Justin Yoo <justin.yoojh@gmail.com>
2022-08-26 19:53:07 +0900

    Update PageComponentBase

--- src/ChromeExtensionV2/Components/PageComponentBase.cs
+++ src/ChromeExtensionV2/Components/PageComponentBase.cs
@@ -3,19 +3,37 @@ using Microsoft.JSInterop;
 
 namespace ChromeExtensionV2.Components
 {
-    public class PageComponentBase : ComponentBase
+    public abstract class PageComponentBase : ComponentBase
     {
+        /// <summary>
+        /// Gets or sets the <see cref="IJSRuntime"/> instance.
+        /// </summary>
         [Inject]
         protected IJSRuntime JS { get; set; }
 
+        /// <summary>
+        /// Gets the <see cref="IJSObjectReference"/> instance.
+        /// </summary>
         protected IJSObjectReference Module { get; private set; }
 
+        /// <inheritdoc />
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
-            this.Module = await JS.InvokeAsync<IJSObjectReference>("import", "./js/main.js").ConfigureAwait(false);
+            if (!firstRender)
+            {
+                return;
+            }
+
+            this.Module = await this.JS.InvokeAsync<IJSObjectReference>("import", "./js/main.js").ConfigureAwait(false);
 
             var src = "js/dist/browser-polyfill.min.js";
             await this.Module.InvokeVoidAsync("loadJs", src).ConfigureAwait(false);
+            await this.LoadAdditionalJsAsync().ConfigureAwait(false);
         }
+
+        /// <summary>
+        /// Loads additional JavaScript resources.
+        /// </summary>
+        protected abstract Task LoadAdditionalJsAsync();
     }
 }
